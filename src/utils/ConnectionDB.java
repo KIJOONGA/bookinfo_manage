44f887ae7d41b6604830aefbfb3531208ae95820
KIJOONGA <56435430+KIJOONGA@users.noreply.github.com>
2021-03-28 22:39:23 +0800

    Add files via upload

---
+++ src/utils/ConnectionDB.java
@@ -0,0 +1,38 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * 连接MySQL
+ * 
+ * @author KIJOONGA
+ *
+ */
+
+public class ConnectionDB {
+
+	private static String url = "jdbc:mysql://localhost:3306/book";
+	private static String userName = "root";
+	private static String password = "KIJOONGA";
+	private static Connection connection = null;
+	
+	public static Connection getConnection() {
+		if (connection == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				connection = DriverManager.getConnection(url, userName, password);
+			} catch (ClassNotFoundException e) {
+				System.out.println("驱动加载失败");
+				e.printStackTrace();
+			} catch (SQLException e) {
+				System.out.println("MySQL连接失败");
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+}
